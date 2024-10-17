923f486d5719a0bc578fbe0d8f8815159b44b772
justinchiu-cohere <justinchiu@cohere.com>
2024-10-17 13:56:03 -0400

    use modal remote

--- app/server.py
+++ app/server.py
@@ -1,37 +1,55 @@
+import asyncio
 import datasets
 import pdb
 import modal
 import subprocess
 import json
 import pathlib
+import modal
+
+app = modal.App("swebench-server")
+
+image = modal.Image.from_registry("bigcodebench/bigcodebench-evaluate")
 
 
-dataset = datasets.load_dataset("bigcode/bigcodebench", split="v0.1.2")
-
-def combine_code_solution(example):
-    example["solution"] = example["complete_prompt"] + example["canonical_solution"]
-    return example
-
-complete_dataset = dataset.map(combine_code_solution)
-
-
-example = complete_dataset[0]
-print("solution")
-print(example["solution"])
-print("test")
-print(example["test"])
-
-with open("scratch/run.py", "w") as f:
-    f.write(example["solution"])
-    f.write("\n")
-    f.write(example["test"])
-
-output = subprocess.check_output(
-    f"pytest scratch/run.py --json-report --json-report-file=scratch/report.json".split()
+@app.function(
+    image=image,
+    cpu=1.0,
+    concurrency_limit=64,
 )
+def run(example):
+    task_id = example["task_id"].replace("/", "-")
 
-with open("scratch/report.json", "r") as f:
-    result = json.loads(f.read())
-    outcomes = [test["outcome"] for test in result["tests"]]
+    rundir = pathlib.Path(f"scratch/{task_id}")
+    rundir.mkdir(exist_ok=True, parents=True)
 
-pdb.set_trace()
+    with (rundir / "run.py").open("w") as f:
+        f.write(example["solution"])
+        f.write("\n")
+        f.write(example["test"])
+
+    output = subprocess.check_output(
+        f"pytest scratch/{task_id}/run.py --json-report --json-report-file=scratch/{task_id}/report.json".split()
+    )
+
+    with (rundir / "report.json").open("r") as f:
+        result = json.loads(f.read())
+        outcomes = [test["outcome"] for test in result["tests"]]
+        return outcomes
+
+
+@app.local_entrypoint()
+async def main():
+    dataset = datasets.load_dataset("bigcode/bigcodebench", split="v0.1.2[:10]")
+
+    def combine_code_solution(example):
+        example["solution"] = example["complete_prompt"] + example["canonical_solution"]
+        return example
+
+    complete_dataset = dataset.map(combine_code_solution)
+
+    futures = []
+    for example in dataset:
+        futures.append(run.remote.aio(example))
+    all_outcomes = asyncio.gather(*futures)
+    pdb.set_trace()
