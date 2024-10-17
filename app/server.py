5155a2e278e314afbebe5b7e3607b2ad652235ea
justinchiu-cohere <justinchiu@cohere.com>
2024-10-17 17:09:16 -0400

    .

--- app/server.py
+++ app/server.py
@@ -80,7 +80,7 @@ web_image = modal.Image.debian_slim(python_version="3.10")
 @modal.web_endpoint(
     method="POST", label=f"runtest",
 )
-async def runtest(data) -> list[list[str]]:
+async def runtest(data: Request) -> list[list[str]]:
     """Generate responses to a batch of prompts, optionally with custom inference settings."""
     futures = []
     for code in data.codes:
